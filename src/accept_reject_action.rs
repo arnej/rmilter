ba1a3ffe8b2e9248d8abb7f64421ee1bc8cbce85
Arne Janbu <ajanbu@gmx.de>
2020-09-29 16:05:34 +0200

    Document AcceptRejectAction

--- src/accept_reject_action.rs
+++ src/accept_reject_action.rs
@@ -1,8 +1,15 @@
+/// Defines the accept/reject actions that the milter returns for each step during the processing
+/// flow.
 pub enum AcceptRejectAction {
+    /// Accept the message without further processing
     Accept,
+    /// Continue processing the message
     Continue,
+    /// Silently discard the message without further processing
     Discard,
+    /// Reject the message without further processing
     Reject,
+    /// Temporarily fail without further processing
     Tempfail,
     // TODO ReplyCode
 }
